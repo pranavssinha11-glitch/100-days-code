b183bd9fdaf9993fb316496ac05607830d43e500
pranavssinha11-glitch <pranavssinha11@gmail.com>
2025-11-06 12:48:53 +0530

    Add files via upload

---
+++ day7q14.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    if ((ch >= 'A' && ch <= 'Z')|| (ch >= 'a' && ch <= 'z')) {
+        if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'||
+            ch=='a' || ch =='e'|| ch=='i' || ch=='o' || ch=='u') {
+                printf("%c is a vowel.\n",ch);
+            }
+            else {
+                printf("%c is a consonant.\n", ch);
+            }
+        }
+        else {
+            printf("Invaild input! Please enter an alphabet.\n");
+
+        }
+            return 0;
+    }
+            
+        
+        
+        
+        
+    
